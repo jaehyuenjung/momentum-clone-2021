8b459d5a49fc3ea3235ab5c7682e6007a86767e7
jaehyuenjung <jaehyuen0613@gmail.com>
2021-07-14 22:53:06 +0900

    Add ToDos List Checked function

--- js/todo.js
+++ js/todo.js
@@ -4,18 +4,54 @@ const input = document.querySelector(".todo-app__input");
 const todoList = document.querySelector(".todo-app__todo-list");
 
 const CLASS_NAME_CONTENT = "todo-app__content";
+const CLASS_NAME_CHECKBOX = "todo-app__check-box";
 
-let isPopup = false;
+let isPopup = 0;
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
+function saveState() {
+    localStorage.setItem("isPopup", JSON.stringify(isPopup));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    toDos = toDos.filter(todo => todo.id != li.id);
+    li.remove();
+    saveToDos();
+}
+
+function paintToDo(newTodoObj) {
+    const li = createTodoEle();
+    li.id = newTodoObj.id;
+    const content = li.querySelector(`.${CLASS_NAME_CONTENT}`);
+    content.innerText = newTodoObj.text;
+    const checkbox = li.querySelector(`.${CLASS_NAME_CHECKBOX}`);
+    checkbox.checked = newTodoObj.isChecked;
+
+    if (newTodoObj.isChecked) {
+        content.classList.add("check-box-line");
+    }
+    todoList.appendChild(li);
+}
 
 function handleTodoControlClick() {
-    if (!isPopup) {
+    isPopup = (isPopup + 1) % 2;
+
+    paintPopup();
+    saveState();
+}
+
+function paintPopup() {
+    if (isPopup) {
         appWrapper.classList.add("start-popup-animation");
         appWrapper.classList.remove("end-popup-animation");
-        isPopup = true;
     } else {
         appWrapper.classList.remove("start-popup-animation");
         appWrapper.classList.add("end-popup-animation");
-        isPopup = false;
     }
 }
 
@@ -29,11 +65,13 @@ function createTodoEle() {
 
     li.className = "todo-app__item";
     checkBoxWrapper.className = "todo-app__check-box-wrapper";
-    checkBox.className = "todo-app__check-box";
+    checkBox.className = CLASS_NAME_CHECKBOX;
     checkBox.type = "checkbox";
     content.className = CLASS_NAME_CONTENT;
     more.className = "todo-app__more";
     i.className = "fas fa-ellipsis-h fa-xs";
+
+    checkBox.addEventListener("click", handleCheckBoxClick);
 
     li.appendChild(checkBoxWrapper);
     li.appendChild(content);
@@ -45,68 +83,51 @@ function createTodoEle() {
 }
 
 function handleToDoInput(event) {
-    const text = input.value;
-    if (text !== "" && event.code === "Enter") {
-        const li = createTodoEle();
-        const content = li.querySelector(`.${CLASS_NAME_CONTENT}`);
-        content.innerText = text;
-        todoList.appendChild(li);
+    const newTodo = input.value;
+    if (newTodo !== "" && event.code === "Enter") {
+        const newTodoObj = {
+            text: newTodo,
+            id: Date.now(),
+            isChecked: 0,
+        };
+
+        toDos.push(newTodoObj);
+        saveToDos();
+
+        paintToDo(newTodoObj);
 
         input.value = "";
     }
 }
 
+function handleCheckBoxClick(event) {
+    const li = event.target.parentElement.parentElement;
+    const content = li.querySelector(`.${CLASS_NAME_CONTENT}`);
+    const checkBox = li.querySelector(`.${CLASS_NAME_CHECKBOX}`);
+
+    const todoObj = toDos.find(e => e.id === parseInt(li.id));
+    todoObj.isChecked = checkBox.checked;
+    content.classList.toggle("check-box-line");
+
+    saveToDos();
+}
+
+const savedState = localStorage.getItem("isPopup");
+const savedToDos = localStorage.getItem("toDos");
+
+if (savedState) {
+    isPopup = JSON.parse(savedState);
+
+    if (isPopup) {
+        paintPopup();
+    }
+}
+
+if (savedToDos) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+
 todoControl.addEventListener("click", handleTodoControlClick);
 input.addEventListener("keypress", handleToDoInput);
-// const toDoForm = document.getElementById("todo-form");
-// const toDoInput = document.querySelector("#todo-form input");
-// const toDoList = document.getElementById("todo-list");
-
-// let toDos = [];
-
-// function saveToDos() {
-//     localStorage.setItem("toDos", JSON.stringify(toDos));
-// }
-
-// function deleteToDo(event) {
-//     const li = event.target.parentElement;
-//     toDos = toDos.filter(todo => todo.id != li.id);
-//     li.remove();
-//     saveToDos();
-// }
-
-// function paintToDo(newTodo) {
-//     const li = document.createElement("li");
-//     li.id = newTodo.id;
-//     const span = document.createElement("span");
-//     span.innerText = newTodo.text;
-//     const button = document.createElement("button");
-//     button.innerText = "❌";
-//     button.addEventListener("click", deleteToDo);
-//     li.appendChild(span);
-//     li.appendChild(button);
-//     toDoList.appendChild(li);
-// }
-
-// function handleToDoSubmit(event) {
-//     event.preventDefault();
-//     const newTodo = toDoInput.value;
-//     toDoInput.value = "";
-//     const newTodoObj = {
-//         text: newTodo,
-//         id: Date.now(),
-//     };
-//     toDos.push(newTodoObj);
-//     paintToDo(newTodoObj);
-//     saveToDos();
-// }
-
-// toDoForm.addEventListener("submit", handleToDoSubmit);
-
-// const savedToDos = localStorage.getItem("toDos");
-
-// if (savedToDos) {
-//     const parsedToDos = JSON.parse(savedToDos);
-//     toDos = parsedToDos;
-//     parsedToDos.forEach(paintToDo);
-// }
